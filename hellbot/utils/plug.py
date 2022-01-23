5b1c5f5a75dddfe4d503bb0567262df84d10daa3
QUEENPRIYAOP <95538576+QUEENPRIYAOP@users.noreply.github.com>
2022-01-23 12:24:07 +0530

    Update plug.py

--- hellbot/utils/plug.py
+++ hellbot/utils/plug.py
@@ -16,17 +16,17 @@ from telethon import events
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from hellbot import *
-from hellbot.clients import *
-from hellbot.helpers import *
-from hellbot.config import *
-from hellbot.utils import *
+from THANOSBOT import *
+from THANOSBOT.clients import *
+from THANOSBOT.helpers import *
+from THANOSBOT.config import *
+from THANOSBOT.utils import *
 
 
 # ENV
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
-    from hellbot.config import Config
+    from THANOSBOT.config import Config
 else:
     if os.path.exists("Config.py"):
         from Config import Development as Config
@@ -37,57 +37,57 @@ def load_module(shortname):
     if shortname.startswith("__"):
         pass
     elif shortname.endswith("_"):
-        import hellbot.utils
+        import THANOSBOT.utils
 
         path = Path(f"hellbot/plugins/{shortname}.py")
-        name = "hellbot.plugins.{}".format(shortname)
+        name = "THANOSBOT.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        LOGS.info("HellBot - Successfully imported " + shortname)
+        LOGS.info("THANOSBOT - Successfully imported " + shortname)
     else:
-        import hellbot.utils
+        import THANOSBOT.utils
 
         path = Path(f"hellbot/plugins/{shortname}.py")
-        name = "hellbot.plugins.{}".format(shortname)
+        name = "THANOSBOT.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
-        mod.bot = Hell
-        mod.H1 = Hell
+        mod.bot = THANOS
+        mod.H1 = THANOS
         mod.H2 = H2
         mod.H3 = H3
         mod.H4 = H4
         mod.H5 = H5
-        mod.Hell = Hell
-        mod.HellBot = HellBot
-        mod.tbot = HellBot
+        mod.THANOS = THANOS
+        mod.THANOSBOT = THANOSBOT
+        mod.tbot = THANOSBOT
         mod.tgbot = bot.tgbot
         mod.command = command
         mod.CmdHelp = CmdHelp
         mod.client_id = client_id
         mod.logger = logging.getLogger(shortname)
         # support for uniborg
-        sys.modules["uniborg.util"] = hellbot.utils
+        sys.modules["uniborg.util"] = THANOSBOT.utils
         mod.Config = Config
         mod.borg = bot
-        mod.hellbot = bot
+        mod.THANOSBOT = bot
         mod.edit_or_reply = edit_or_reply
         mod.eor = edit_or_reply
         mod.delete_hell = delete_hell
         mod.eod = delete_hell
         mod.Var = Config
         mod.admin_cmd = admin_cmd
-        mod.hell_cmd = hell_cmd
+        mod.THANOS_cmd = THANOS_cmd
         mod.sudo_cmd = sudo_cmd
         # support for other userbots
-        sys.modules["userbot.utils"] = hellbot.utils
-        sys.modules["userbot"] = hellbot
+        sys.modules["userbot.utils"] = THANOSBOT.utils
+        sys.modules["userbot"] = THANOSBOT
         # support for paperplaneextended
-        sys.modules["userbot.events"] = hellbot
+        sys.modules["userbot.events"] = THANOSBOT
         spec.loader.exec_module(mod)
         # for imports
-        sys.modules["hellbot.plugins." + shortname] = mod
-        LOGS.info("⚡ Hêllẞø† ⚡ - Successfully Imported " + shortname)
+        sys.modules["THANOSBOT.plugins." + shortname] = mod
+        LOGS.info("⚡ THANOSBOT† ⚡ - Successfully Imported " + shortname)
 
 
 # remove plugins
@@ -99,7 +99,7 @@ def remove_plugin(shortname):
             del LOAD_PLUG[shortname]
 
         except BaseException:
-            name = f"hellbot.plugins.{shortname}"
+            name = f"THANOSBOT.plugins.{shortname}"
 
             for i in reversed(range(len(bot._event_builders))):
                 ev, cb = bot._event_builders[i]
@@ -108,4 +108,4 @@ def remove_plugin(shortname):
     except BaseException:
         raise ValueError
 
-# hellbot
+# THANOSBOT
