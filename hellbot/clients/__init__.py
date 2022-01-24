90fb4938a07d734d4cadeedc146594fbddc7d6d7
QUEENPRIYAOP <95538576+QUEENPRIYAOP@users.noreply.github.com>
2022-01-24 12:07:51 +0530

    Update __init__.py

--- hellbot/clients/__init__.py
+++ hellbot/clients/__init__.py
@@ -1,3 +1,3 @@
 from .client_list import clients_list, client_id
-from .decs import hell_cmd, hell_handler
-from .session import Hell, H2, H3, H4, H5, HellBot
+from .decs import THANOS_cmd, THANOS_handler
+from .session import THANOS, H2, H3, H4, H5, THANOSBOT
