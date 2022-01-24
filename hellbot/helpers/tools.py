f71647bcca950611b8c263e85437030abb8f3ae4
QUEENPRIYAOP <95538576+QUEENPRIYAOP@users.noreply.github.com>
2022-01-24 11:48:28 +0530

    Update tools.py

--- hellbot/helpers/tools.py
+++ hellbot/helpers/tools.py
@@ -3,8 +3,8 @@ import re
 
 from telethon import events
 
-from hellbot import bot
-from hellbot.config import Config
+from THANOSBOT import bot
+from THANOSBOT.config import Config
 
 
 # forward check
